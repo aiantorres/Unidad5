2b9d506320c34cf2edaabc344595fe43be215110
aiantorres <aiantorres@users.noreply.github.com>
2023-02-02 17:26:40 +0100

    v

---
+++ JAvascript/ejercicio2.js
@@ -0,0 +1,5 @@
+const celta = {
+    día: 2,
+    mes: 'septiembre'
+};
+console.log(`Hola mi cumpleaños es el ${celta.día} de ${celta.mes}`);
---
+++ JAvascript/ejercicio3.js
@@ -0,0 +1,14 @@
+const numero1 = 5;
+const numero2 = 8;
+ if (numero2>numero1){
+    alert(numero1 +' no es mayor que '+numero2);
+ };
+ if (numero1>=0){
+    alert(numero1 +' es positivo ');
+ };
+ if (numero1<0){
+    alert(numero1 +' es negativo o distinto de cero ');
+ };
+ if (numero1 + 1 <= numero2){
+    alert('El incrementar en 1 unidad el valor de ' + numero1 + ' no lo hace mayor o igual que' + numero2);
+ };
